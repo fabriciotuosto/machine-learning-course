838199f672272d2590f55af179391787fb7c1a98
Fabricio Tuosto <fabricio@scvsoft.com>
2015-07-31 22:18:44 -0300

    Learninc Curve Validate Curve

--- TP5/linearRegCostFunction.m
+++ TP5/linearRegCostFunction.m
@@ -4,8 +4,6 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
     %   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
     %   cost of using theta as the parameter for linear regression to fit the 
     %   data points in X and y. Returns the cost in J and the gradient in grad
-
-    % Initialize some useful values
     h_x = X * theta;           % Prediction of hypoteshis on all values
     J = linear_regression_cost(h_x, y) + linear_regression_reg(y, theta, lambda);
     grad = linear_gradient(X, h_x, y)' + linear_gradient_reg(y, theta, lambda);
