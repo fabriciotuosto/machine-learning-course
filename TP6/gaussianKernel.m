d9299efa9a46d13232fe1e9f3131f4f458f1dd1e
Fabricio Tuosto <fabricio@scvsoft.com>
2015-08-01 18:51:09 -0300

    Gaussian Kernel

--- TP6/gaussianKernel.m
+++ TP6/gaussianKernel.m
@@ -1,26 +1,11 @@
 function sim = gaussianKernel(x1, x2, sigma)
-%RBFKERNEL returns a radial basis function kernel between x1 and x2
-%   sim = gaussianKernel(x1, x2) returns a gaussian kernel between x1 and x2
-%   and returns the value in sim
-
-% Ensure that x1 and x2 are column vectors
-x1 = x1(:); x2 = x2(:);
-
-% You need to return the following variables correctly.
-sim = 0;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the similarity between x1
-%               and x2 computed using a Gaussian kernel with bandwidth
-%               sigma
-%
-%
-
-
-
-
-
-
-% =============================================================
-    
+    %RBFKERNEL returns a radial basis function kernel between x1 and x2
+    %   sim = gaussianKernel(x1, x2) returns a gaussian kernel between x1 and x2
+    %   and returns the value in sim
+    % Ensure that x1 and x2 are column vectors
+    x1 = x1(:); x2 = x2(:);
+    diff = x1 - x2;
+    n = diff' * diff;
+    std_dv = 2 * (sigma^2);
+    sim = exp(-n/std_dv);
 end
