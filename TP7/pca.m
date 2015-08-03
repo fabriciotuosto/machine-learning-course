7efef2796218f72dc031433179ed3d13b93aacde
Fabricio Tuosto <fabricio@scvsoft.com>
2015-08-03 00:21:53 -0300

    Refactor PSA Added missing obtaining random K Menas Centroid * The grader doesn't have this into account

--- TP7/pca.m
+++ TP7/pca.m
@@ -3,7 +3,6 @@ function [U, S] = pca(X)
     %   [U, S, X] = pca(X) computes eigenvectors of the covariance matrix of X
     %   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
     %
-    [m, ~] = size(X);
-    Sigma = (X' * X)/m;
+    Sigma = (X' * X)/(size(X, 1));
     [U, S, ~] = svd(Sigma);
 end
